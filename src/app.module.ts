9015a1f226327c7378b22e501a0da3733fbe35e8
Arthur Gomes <97976189+arthur-cgomes@users.noreply.github.com>
2022-10-06 18:00:22 -0300

    add crud user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ormConfig } from './ormconfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormConfig),
+    UserModule,
   ],
 })
 export class AppModule {}
