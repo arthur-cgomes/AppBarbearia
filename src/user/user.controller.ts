b22d66142661416463cb503f2b4c68aac0d48a18
Arthur Gomes <97976189+arthur-cgomes@users.noreply.github.com>
2022-10-07 20:45:20 -0300

    add update/get id user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { ApiConflictResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Body, Put, Param, Get } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiConflictResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
 @ApiTags('User')
@@ -23,4 +24,33 @@ export class UserController {
     ) {
         return await this.userService.createUser(createUserDto);
     }
+
+    @Put(':userId')
+    @ApiOperation({
+        summary: 'Atualiza um usuário',
+    })
+    //@ApiOkResponse({ type: UserDto })
+    @ApiNotFoundResponse({ description: 'Usuário não encontrado' })
+    @ApiBadRequestResponse({
+        description: 'Dados inválidos',
+    })
+    async updateUser(
+        @Param('userId') userId: string,
+        @Body() updateUserDto: UpdateUserDto,
+    ) {
+        return await this.userService.updateUser(userId, updateUserDto);
+    }
+
+    @Get(':userId')
+    @ApiOperation({
+        summary: 'Retorna um usuário pelo id',
+    })
+    //@ApiOkResponse({ type: UserDto })
+    @ApiNotFoundResponse({ description: 'Usuário não encontrado' })
+    async getUserById(
+        @Param('userId') userId: string
+    ) {
+        return await this.userService.getUserById(userId);
+    }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entity/user.entity';
 
 @Injectable()
@@ -18,9 +19,44 @@ export class UserService {
         });
 
         if (checkUser) {
-            throw new Error('User already exists');
+            throw new ConflictException('user already exists');
         }
 
         return await this.userRepository.create(createUserDto).save();
     }
+
+    public async updateUser(userId: string, updateUserDto: UpdateUserDto): Promise<User> {
+        const user = await this.userRepository.findOne({
+            where: [
+                { id: userId },
+            ],
+        });
+
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+
+        return await (await this.userRepository.preload({
+            id: userId,
+            ...updateUserDto,
+        })
+        ).save();
+    }
+
+    public async getUserById(userId: string): Promise<User> {
+        const user = await this.userRepository.findOne({
+            where: [
+                { id: userId },
+            ],
+        });
+
+        if (!user) {
+            throw new NotFoundException('user with this id not found');
+          }
+      
+
+        return user;
+    }
+
+
 }
