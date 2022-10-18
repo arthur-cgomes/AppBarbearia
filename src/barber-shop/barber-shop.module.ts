c47aa5450a0d9fe64b2be3c99311021e62ebce13
Yuri Mancini <yurimancinidevs@gmail.com>
2022-10-17 22:08:22 -0300

    create services
    Criado os serviços do crud

--- src/barber-shop/barber-shop.module.ts
+++ src/barber-shop/barber-shop.module.ts
@@ -1,9 +1,11 @@
 import { BarberShopController } from './barber-shop.controller';
 import { BarberShopService } from './barber-shop.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BarberShop } from './entity/barber-shop.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([BarberShop])],
   controllers: [BarberShopController],
   providers: [BarberShopService],
 })
--- src/barber-shop/barber-shop.service.ts
+++ src/barber-shop/barber-shop.service.ts
@@ -1,4 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindManyOptions, ILike, In, Repository } from 'typeorm';
+import { CreateBarberShopDto } from './dto/create-barbershop.dto';
+import { UpdateBarberShopDto } from './dto/update-barbershop.dto';
+import { BarberShop } from './entity/barber-shop.entity';
 
 @Injectable()
-export class BarberShopService {}
+export class BarberShopService {
+  constructor(
+    @InjectRepository(BarberShop)
+    private readonly barbershopRepository: Repository<BarberShop>,
+  ) {}
+
+  public async createBarberShop(
+    createBarberShopDto: CreateBarberShopDto,
+  ): Promise<BarberShop> {
+    const checkBarberShop = await this.barbershopRepository.findOne({
+      where: [{ name: createBarberShopDto.name }], //esta buscando somente o nome, tera mais filtros?
+    });
+
+    if (checkBarberShop)
+      throw new ConflictException('barbershop already exists with this name'); //verificar o metodo de erro
+
+    return await this.barbershopRepository
+      .create({ ...createBarberShopDto })
+      .save();
+  }
+
+  public async updateBarberShop(
+    id: string,
+    updateBarberShopDto: UpdateBarberShopDto,
+  ): Promise<BarberShop> {
+    await this.getBarberShopById(id);
+
+    return await (
+      await this.barbershopRepository.preload({ id, ...updateBarberShopDto })
+    ).save();
+  }
+
+  public async getBarberShopById(id: string): Promise<BarberShop> {
+    const barbershop = await this.barbershopRepository.findOne({
+      where: { id },
+    });
+
+    if (!barbershop)
+      throw new NotFoundException('barbershop with id not found');
+
+    return barbershop;
+  }
+
+  public async getBarberShopByIds(ids: string[]): Promise<BarberShop[]> {
+    return await this.barbershopRepository.findBy({ id: In(ids) });
+  }
+
+  public async getAllBarberShop(
+    take: number,
+    skip: number,
+    search?: string,
+  ): Promise<{
+    skip: number;
+    total: number;
+    barbershops: BarberShop[];
+  }> {
+    const conditions: FindManyOptions<BarberShop> = {
+      take,
+      skip,
+    };
+
+    if (search) {
+      conditions.where = { name: ILike('%' + search + '%') };
+    }
+
+    const [barbershops, count] = await this.barbershopRepository.findAndCount(
+      conditions,
+    );
+
+    if (barbershops.length == 0) {
+      return { skip: null, total: 0, barbershops };
+    }
+    const over = count - Number(take) - Number(skip);
+    skip = over <= 0 ? null : Number(skip) + Number(take);
+
+    return { skip, total: count, barbershops };
+  }
+
+  public async deleteBarberShop(id: string): Promise<BarberShop> {
+    const barbershop = await this.getBarberShopById(id);
+
+    await this.barbershopRepository.remove(barbershop);
+
+    return barbershop;                  //mandei retornar o barbershop escluido, verificar se deve retornar uma mesagem 
+  }
+}
