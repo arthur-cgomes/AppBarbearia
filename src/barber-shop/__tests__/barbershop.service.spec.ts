030fa03b14c343b6a8b7d8bbf9775eb9f271dbc0
Yuri Mancini <yurimancinidevs@gmail.com>
2022-10-18 23:25:13 -0300

    teste de criação da barbershop

--- src/barber-shop/__tests__/barbershop.service.spec.ts
+++ src/barber-shop/__tests__/barbershop.service.spec.ts
@@ -4,6 +4,8 @@ import { MockRepository, repositoryMockFactory } from "src/utils/mock/test.util"
 import { Repository } from "typeorm";
 import { BarberShopService } from "../barber-shop.service"
 import { BarberShop } from "../entity/barber-shop.entity";
+import { CreateBarberShopDto } from "../dto/create-barbershop.dto"
+import { ConflictException } from "@nestjs/common";
 
 describe('BarberShopService', () => {
     let service: BarberShopService;
@@ -22,6 +24,44 @@ describe('BarberShopService', () => {
 
         service = module.get<BarberShopService>(BarberShopService);
         repositoryMock = module.get(getRepositoryToken(BarberShop));
+    });
+
+    beforeEach(() => jest.clearAllMocks());
+
+    const barbershop: BarberShop = {
+        id: '12314-121454-ç687ih',
+        name: 'Teste',
+        active: true,
+    } as BarberShop;
+
+    describe('createbarbershop', () => {
+        const createBarbershopDto: CreateBarberShopDto = {
+            name: 'Teste',
+        };
+
+        it('Should successfully create a barbershop', async () => {
+            repositoryMock.findOne = jest.fn();
+            repositoryMock.create = jest.fn().mockReturnValue({ save: () => barbershop });
+
+            const result = await service.createBarberShop(createBarbershopDto);
+
+            expect(result).toStrictEqual(barbershop);
+            expect(repositoryMock.create).toHaveBeenCalledWith({
+                ...createBarbershopDto,
+            });
+        });
+
+        it('Should throw a ConflictException if barbershop already exists', async () => {
+            const error = new ConflictException(
+                'barbershop already exists with this name',
+            );
+            repositoryMock.findOne = jest.fn().mockReturnValue(barbershop);
+
+            await expect(
+                service.createBarberShop(createBarbershopDto),
+            ).rejects.toStrictEqual(error);
+            expect(repositoryMock.create).not.toHaveBeenCalled();
+        });
     });
 
     
