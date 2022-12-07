7689241f0ceefbd834093b215399ec55fba13b2d
Yuri Mancini <97924769+YMdv@users.noreply.github.com>
2022-12-07 20:05:05 -0300

    base barber

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UserTypeModule } from './user-type/user-type.module';
 import { NotificationModule } from './notification/notification.module';
 import { UserNotificationModule } from './user-notification/user-notification.module';
 import { ServicesModule } from './services/services.module';
+import { BarberModule } from './barber/barber.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ServicesModule } from './services/services.module';
     UserNotificationModule,
     BarberShopModule,
     ServicesModule,
+    BarberModule,
   ],
 })
 export class AppModule {}
