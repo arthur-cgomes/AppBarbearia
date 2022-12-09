de137ff27b0f3901f6d897b683ff4b434f5a24df
Yuri Mancini <97924769+YMdv@users.noreply.github.com>
2022-12-09 15:16:31 -0300

    entidade barber

---
+++ src/barber/entity/barber.entity.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToOne } from "typeorm";
+import { BarberShop } from "../../barber-shop/entity/barber-shop.entity";
+import { BaseCollection } from "../../common/entity/base.entity";
+
+@Entity()
+export class Barber extends BaseCollection {
+    @ApiProperty()
+    @IsNotEmpty()
+    @Column({ length: 255 })
+    name: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @Column({ length: 20, default: null })
+    phone: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @OneToOne(() => BarberShop, (barbershop) => barbershop.barber)
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    format = () => {
+      if (this.phone) this.phone = this.phone.replace(/[^\d]+/g, '');
+    };
+}
