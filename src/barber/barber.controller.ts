d559f6b69cff24e148a9c6b6697a2f240f953a81
Yuri Mancini <yurimancinidevs@gmail.com>
2023-01-09 22:00:02 -0300

    configuração do jest + roda post do controller
    alteração no jest, desabilitando a função de rodar os testes  a cada alteração e inicio do controlador, com a rota de post, para criar um barbeiro

--- src/barber/barber.controller.ts
+++ src/barber/barber.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { BarberService } from './barber.service';
+import { BarberDto } from './dto/barber.dto';
+import { CreateBarberDto } from './dto/create-barber.dto';
 
+@ApiTags('Barber')
 @Controller('barber')
-export class BarberController {}
+export class BarberController {
+  constructor(private readonly barberService: BarberService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criar um novo barbeiro',
+  })
+  @ApiCreatedResponse({
+    type: BarberDto,
+  })
+  @ApiConflictResponse({
+    description: 'Barbeariaa já cadastrada',
+  })
+  async createbarber(@Body() createBarberDto: CreateBarberDto) {
+    return await this.barberService.createBarber(createBarberDto);
+  }
+}
