aaae85c16797c54d856dac095bc979da09b4ffaa
Arthur Gomes <97976189+arthur-cgomes@users.noreply.github.com>
2023-01-23 18:31:43 -0300

    add updateBarber/getById service/controller

--- src/barber/barber.controller.ts
+++ src/barber/barber.controller.ts
@@ -1,35 +1,77 @@
-import { Controller, Post, Body, Delete, Param } from '@nestjs/common';
-import { ApiConflictResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+  Get,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { DeleteResponseDto } from 'src/common/dto/delete-response.dto';
 import { BarberService } from './barber.service';
 import { CreateBarberDto } from './dto/create-barber.dto';
+import { UpdateBarberDto } from './dto/update-barber.dto';
 
 @ApiTags('Barber')
 @Controller('barbers')
 export class BarberController {
-    constructor(
-        private readonly barberService: BarberService
-    ) { }
+  constructor(private readonly barberService: BarberService) {}
 
-    @Post()
-    @ApiOperation({
-        summary: 'Adiona um novo barbeiro',
-    })
-    //@ApiCreatedResponse({ type: * })
-    @ApiConflictResponse({
-        description: 'Barbeiro já cadastrado',
-    })
-    async createBarber(@Body() createBarberDto: CreateBarberDto) {
-        return await this.barberService.createBarber(createBarberDto);
-    }
+  @Post()
+  @ApiOperation({
+    summary: 'Adiona um novo barbeiro',
+  })
+  //@ApiCreatedResponse({ type: * })
+  @ApiConflictResponse({
+    description: 'Barbeiro já cadastrado',
+  })
+  async createBarber(@Body() createBarberDto: CreateBarberDto) {
+    return await this.barberService.createBarber(createBarberDto);
+  }
 
-    @Delete(':barberId')
-    @ApiOperation({
-        summary: 'Exclui um barbeiro',
-    })
-    @ApiOkResponse({ type: DeleteResponseDto })
-    @ApiNotFoundResponse({ description: 'Barbeiro não encontrado' })
-    async deleteBarber(@Param('barberId') barberId: string) {
-        return { message: await this.barberService.deleteBarber(barberId) };
-    }
+  @Put(':barberId')
+  @ApiOperation({
+    summary: 'Atualiza um barbeiro',
+  })
+  //@ApiOkResponse({ type: * })
+  @ApiNotFoundResponse({ description: 'Barbeiro não encontrado' })
+  @ApiBadRequestResponse({
+    description: 'Dados inválidos',
+  })
+  async updateBarber(
+    @Param('barberId') barberId: string,
+    @Body() updateBarberDto: UpdateBarberDto,
+  ) {
+    return await this.barberService.updateBarber(barberId, updateBarberDto);
+  }
+
+  @Get(':barberId')
+  @ApiOperation({
+    summary: 'Retorna um barbeiro pelo id',
+  })
+  //@ApiOkResponse({ type: * })
+  @ApiNotFoundResponse({ description: 'Barbeiro não encontrado' })
+  async getBarberbyId(@Param('barberId') barberId: string) {
+    return await this.barberService.getBarberbyId(barberId);
+  }
+
+  //Fazer o getAllBarbers
+
+  @Delete(':barberId')
+  @ApiOperation({
+    summary: 'Exclui um barbeiro',
+  })
+  @ApiOkResponse({ type: DeleteResponseDto })
+  @ApiNotFoundResponse({ description: 'Barbeiro não encontrado' })
+  async deleteBarber(@Param('barberId') barberId: string) {
+    return { message: await this.barberService.deleteBarber(barberId) };
+  }
 }
--- src/barber/barber.service.ts
+++ src/barber/barber.service.ts
@@ -1,48 +1,84 @@
-import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateBarberDto } from './dto/create-barber.dto';
+import { UpdateBarberDto } from './dto/update-barber.dto';
 import { Barber } from './entity/barber.entity';
 
 @Injectable()
 export class BarberService {
-    constructor(
-        @InjectRepository(Barber) private readonly barberRepository: Repository<Barber>,
-    ) { }
+  constructor(
+    @InjectRepository(Barber)
+    private readonly barberRepository: Repository<Barber>,
+  ) {}
 
-    public async createBarber(createBarberDto: CreateBarberDto): Promise<Barber> {
-        const cpf = await this.barberRepository.findOne({
-            where: [{ cpf: createBarberDto.cpf }],
-        });
-        if (cpf) {
-            throw new ConflictException('barber already exists');
-        }
-
-        const email = await this.barberRepository.findOne({
-            where: [{ email: createBarberDto.email }],
-        });
-        if (email) {
-            throw new ConflictException('email already exists');
-        }
-
-        const phone = await this.barberRepository.findOne({
-            where: [{ phone: createBarberDto.phone }],
-        });
-        if (phone) {
-            throw new ConflictException('phone already exists');
-        }
-
-        return await this.barberRepository.create({ ...createBarberDto }).save();
+  public async createBarber(createBarberDto: CreateBarberDto): Promise<Barber> {
+    const cpf = await this.barberRepository.findOne({
+      where: [{ cpf: createBarberDto.cpf }],
+    });
+    if (cpf) {
+      throw new ConflictException('barber already exists');
     }
 
-    public async deleteBarber(barberId: string): Promise<string> {
-        const barber = await this.barberRepository.findOne({
-            where: [{ id: barberId }],
-        });
-        if (!barber) {
-            throw new NotFoundException('barber not found');
-        }
-        await this.barberRepository.remove(barber);
-        return 'removed';
+    const email = await this.barberRepository.findOne({
+      where: [{ email: createBarberDto.email }],
+    });
+    if (email) {
+      throw new ConflictException('email already exists');
     }
+
+    const phone = await this.barberRepository.findOne({
+      where: [{ phone: createBarberDto.phone }],
+    });
+    if (phone) {
+      throw new ConflictException('phone already exists');
+    }
+
+    return await this.barberRepository.create({ ...createBarberDto }).save();
+  }
+
+  public async updateBarber(
+    barberId: string,
+    updateBarberDto: UpdateBarberDto,
+  ): Promise<Barber> {
+    const barber = await this.barberRepository.findOne({
+      where: [{ id: barberId }],
+    });
+    if (!barber) {
+      throw new NotFoundException('barber not found');
+    }
+    return await (
+      await this.barberRepository.preload({
+        id: barberId,
+        ...updateBarberDto,
+      })
+    ).save();
+  }
+
+  public async getBarberbyId(barberId: string): Promise<Barber> {
+    const barber = await this.barberRepository.findOne({
+      where: [{ id: barberId }],
+    });
+    if (!barber) {
+      throw new NotFoundException('barber not found');
+    }
+    return barber;
+  }
+
+  //Fazer o getAllBarbers
+
+  public async deleteBarber(barberId: string): Promise<string> {
+    const barber = await this.barberRepository.findOne({
+      where: [{ id: barberId }],
+    });
+    if (!barber) {
+      throw new NotFoundException('barber not found');
+    }
+    await this.barberRepository.remove(barber);
+    return 'removed';
+  }
 }
